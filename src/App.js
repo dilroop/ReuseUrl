50cef1df34d5bb49a90ff883ed8d9b6a9c6bcdde
Dilroop Singh <Dilroop.singh@gmail.com>
2019-10-01 22:02:13 +1300

    Smile Componant Added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Smile from "./componants/Smile"
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Smile />
+        <h1>Hello Might</h1>
+        <h3>Aakash Bista </h3> 
+        &amp; 
+        <h3> Parth Patel</h3>
       </header>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/componants/Smile.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Smile extends Component {
+    render() { 
+        return (
+            <h1>:) :)</h1>
+          );
+    }
+}
+ 
+export default Smile;
