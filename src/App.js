36b708731826ee3d51f38cf90ca4a15ab11df443
Dilroop Singh <Dilroop.singh@gmail.com>
2019-10-06 01:15:25 +1300

    theme added and app bar added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from "react";
-import "./App.css";
-import Smile from "./componants/Smile"
+import AppBar from "./componants/AppBar";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import reurseUrlTheme from "./reurseUrlTheme";
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Smile />
+    <MuiThemeProvider theme={reurseUrlTheme}>
+      <div>
+        <AppBar />
         <h1>Hello Might</h1>
-        <h3>Aakash Bista </h3> 
-        &amp; 
+        <h3>Aakash Bista </h3>
+        &amp;
         <h3> Parth Patel</h3>
-      </header>
-    </div>
+      </div>
+    </MuiThemeProvider>
   );
 };
 
---
+++ src/reurseUrlTheme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const reurseUrlTheme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#2979ff"
+    },
+    secondary: {
+      main: "#512da8"
+    }
+  }
+});
+
+export default reurseUrlTheme;
