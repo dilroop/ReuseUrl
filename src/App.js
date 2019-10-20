6c65066173d6c9834c63293163f8d12351b0f72e
Dilroop Singh <Dilroop.singh@gmail.com>
2019-10-20 18:22:29 +1300

    router added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import { ThemeProvider } from "@material-ui/styles";
 import React from "react";
 import AppBar from "./components/AppBar";
 import InfoContent from "./components/home/InfoContent";
+import Footer from "./components/home/Footer";
 import reuseUrlTheme from "./reuseUrlTheme";
+import { Switch, Route } from "react-router-dom";
 
 const App = () => {
     return (
         <ThemeProvider theme={reuseUrlTheme}>
             <AppBar />
-            <InfoContent />
+            <Switch>
+                <Route exact path="/" component={InfoContent} />
+                <Route exact path="/home" component={InfoContent} />
+                <Route path="/footer" component={Footer} />
+            </Switch>
         </ThemeProvider>
     );
 };
